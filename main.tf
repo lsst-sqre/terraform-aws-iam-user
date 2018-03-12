4db38101a6ddc3ff21e5e85d068cd76a6ee6c278
Joshua Hoblitt <josh@hoblitt.com>
2018-03-12 11:02:17 -0700

    general tf style seem prefers _ over - in resource name

--- main.tf
+++ main.tf
@@ -6,31 +6,31 @@ variable "policy" {
   description = "IAM policy to attach to IAM user"
 }
 
-resource "aws_iam_user" "iam-user" {
+resource "aws_iam_user" "iam_user" {
   name = "${var.name}"
 }
 
-resource "aws_iam_access_key" "iam-user" {
-  user = "${aws_iam_user.iam-user.name}"
+resource "aws_iam_access_key" "iam_user" {
+  user = "${aws_iam_user.iam_user.name}"
 }
 
-resource "aws_iam_user_policy" "iam-user" {
-  name   = "${aws_iam_user.iam-user.name}-policy"
-  user   = "${aws_iam_user.iam-user.name}"
+resource "aws_iam_user_policy" "iam_user" {
+  name   = "${aws_iam_user.iam_user.name}-policy"
+  user   = "${aws_iam_user.iam_user.name}"
   policy = "${var.policy}"
 }
 
 output "name" {
   sensitive = true
-  value     = "${aws_iam_access_key.iam-user.user}"
+  value     = "${aws_iam_access_key.iam_user.user}"
 }
 
 output "id" {
   sensitive = true
-  value     = "${aws_iam_access_key.iam-user.id}"
+  value     = "${aws_iam_access_key.iam_user.id}"
 }
 
 output "secret" {
   sensitive = true
-  value     = "${aws_iam_access_key.iam-user.secret}"
+  value     = "${aws_iam_access_key.iam_user.secret}"
 }
